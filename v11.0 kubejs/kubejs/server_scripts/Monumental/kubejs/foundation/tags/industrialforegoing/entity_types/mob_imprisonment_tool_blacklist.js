f286120aff9f9f83796d16d499e79cc8b23fc1d9
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-08-01 23:30:19 -0500

    Fixing File Format

--- v11.0 kubejs/kubejs/server_scripts/Monumental/kubejs/foundation/tags/industrialforegoing/entity_types/mob_imprisonment_tool_blacklist.js
+++
@@ -1,4 +0,0 @@
-onEvent('item.tags', event => {
-// Adds items to the Wither Immune Tag
-  event.add('industrialforegoing:mob_imprisonment_tool_blacklist', /iceandfire:.+_dragon/)
-})
--- v11.0 kubejs/kubejs/server_scripts/Monumental/kubejs/foundation/tags/items/forge/heart.js
+++
@@ -1,9 +0,0 @@
-onEvent('item.tags', (event) => {
-    event.add('forge:heart', [
-        'iceandfire:fire_dragon_heart',
-        'iceandfire:ice_dragon_heart',
-        'iceandfire:lightning_dragon_heart',
-        'iceandfire:hydra_heart',
-        'losttrinkets:blaze_heart'
-    ]);
-});
