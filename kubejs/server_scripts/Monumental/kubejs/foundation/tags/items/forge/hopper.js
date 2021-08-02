ea39f748ff1231dc6806d14548b1635e5238f0ca
ModernGamingWorld <ModernGamingWorld@outlook.com>
2021-08-01 23:29:23 -0500

    Fixed File Location on GitHub

--- kubejs/server_scripts/Monumental/kubejs/foundation/tags/items/forge/hopper.js
+++
@@ -1,9 +0,0 @@
-onEvent('item.tags', (event) => {
-    event.add('forge:hopper', [
-        'minecraft:hopper',
-        'naturesaura:grated_chute',
-        'pneumaticcraft:omnidirectional_hopper',
-        'pneumaticcraft:liquid_hopper',
-        'woodenutilities:wooden_hopper'
-    ]);
-});
