56d787c9774fdf33c9423714ef5615bcb4944691
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2021-09-20 19:36:36 -0500

    Create blockdrops.js

---
+++ kubejs/server_scripts/monumental/kubejs/foundation/blockdrops.js
@@ -0,0 +1,19 @@
+//BLOCK DROP LOOT TABLE SCRIPT
+//BY ModernGamingWorld / 2021
+//
+//Makes vanilla and modded Blocks drop Mystical Agriculture Essence
+//
+onEvent('block.loot_tables', event => {
+
+    //Wooden Essences
+    event.addSimpleBlock('#minecraft:logs', 'mysticalagriculture:wood_essence')
+
+    //Dirt Essences
+    event.addSimpleBlock('#forge:dirt', 'mysticalagriculture:dirt_essence')
+
+    //Nether Essences
+    event.addSimpleBlock('#forge:netherrack', 'mysticalagriculture:nether_essence')
+
+    //End Essences
+    event.addSimpleBlock('#forge:end_stones', 'mysticalagriculture:end_essence')
+})
