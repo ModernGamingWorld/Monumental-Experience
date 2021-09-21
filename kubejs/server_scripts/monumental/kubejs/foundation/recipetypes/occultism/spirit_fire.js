b11a0add0caaf4002705b95de37431d7db294982
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2021-09-21 11:07:13 -0500

    Create spirit_fire.js

---
+++ kubejs/server_scripts/monumental/kubejs/foundation/recipetypes/occultism/spirit_fire.js
@@ -0,0 +1,47 @@
+onEvent('recipes', (event) => {
+    const recipes = [
+        {
+            input: {
+                item: 'tconstruct:blaze_head'
+            },
+            output: 'mysticalagriculture:blaze_essence',
+            id: 'occultism:spirit_fire/blaze_essence'
+        },
+        {
+            input: {
+                item: 'tconstruct:enderman_head'
+            },
+            output: 'mysticalagriculture:enderman_essence',
+            id: 'occultism:spirit_fire/enderman_essence'
+        },
+        {
+            input: {
+                item: 'minecraft:creeper_head'
+            },
+            output: 'mysticalagriculture:creeper_essence',
+            id: 'occultism:spirit_fire/creeper_essence'
+        },
+        {
+            input: {
+                item: 'minecraft:zombie_head'
+            },
+            output: 'mysticalagriculture:zombie_essence',
+            id: 'occultism:spirit_fire/zombie_essence'
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        let re = event.custom({
+            type: 'occultism:spirit_fire',
+            ingredient: recipe.input,
+            result: {
+                item: recipe.output,
+                count: recipe.count
+            },
+            crushing_time: recipe.time
+        });
+        if (recipe.id) {
+            re.id(recipe.id);
+        }
+    });
+});
