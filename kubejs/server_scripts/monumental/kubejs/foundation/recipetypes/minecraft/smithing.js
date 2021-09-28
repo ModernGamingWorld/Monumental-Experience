e27537bfffd8cd661d8f85cdb7df5229699aeea0
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2021-09-28 02:31:58 -0500

    Update smithing.js

--- kubejs/server_scripts/monumental/kubejs/foundation/recipetypes/minecraft/smithing.js
+++ kubejs/server_scripts/monumental/kubejs/foundation/recipetypes/minecraft/smithing.js
@@ -1,4 +1,6 @@
 onEvent('recipes', event => {
     //event.smithing('Output', 'Input1', 'Input2')
     event.smithing('doom:argent_plate', 'doom:argent_energy', '#forge:ingots/netherite')
+    //Pickaxes
+    event.smithing(Item.of('botania:manasteel_pick', '{Damage:0,AS_Amulet_Holder:[I;955663911,-1640609137,-1846237562,1038191077]}'), '#forge:ingots/manasteel', 'botania:livingwood_twig')
 })
