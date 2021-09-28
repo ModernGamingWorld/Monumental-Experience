424547dffa07fcb3f4a283490177b796e2150964
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2021-09-28 00:37:35 -0500

    changes

--- kubejs/server_scripts/Monumental/kubejs/foundation/tags/items/forge/piston.js
+++
@@ -1,8 +0,0 @@
-onEvent('item.tags', (event) => {
-    event.add('forge:piston', [
-        'minecraft:sticky_piston',
-        'minecraft:piston',
-        'create:mechanical_piston',
-        'create:sticky_mechanical_piston'
-    ]);
-});
