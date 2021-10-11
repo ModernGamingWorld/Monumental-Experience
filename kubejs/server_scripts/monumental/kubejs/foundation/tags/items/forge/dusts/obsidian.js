4afb2a8253f36d59e9f97f5e63208cc816c0ef74
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2021-10-11 02:24:20 -0500

    Update obsidian.js

--- kubejs/server_scripts/monumental/kubejs/foundation/tags/items/forge/dusts/obsidian.js
+++ kubejs/server_scripts/monumental/kubejs/foundation/tags/items/forge/dusts/obsidian.js
@@ -1,5 +1,5 @@
 onEvent('item.tags', (event) => {
      event.remove('forge:dusts/obsidian', 'mekanism:dust_obsidian')
      event.remove('forge:dusts/obsidian', 'occultism:obsidian_dust')
-     event.remove('forge:dusts/obsidian', 'create:powder_obsidian')
+     event.remove('forge:dusts/obsidian', 'create:powdered_obsidian')
 });
