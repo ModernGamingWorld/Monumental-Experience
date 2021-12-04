a5c900f443db85eaa13ad29e5a58171611195def
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2021-12-04 02:05:02 -0600

    Start of 1.18 stuff

---
+++ 1.18/kubejs/client_scripts/Monumental/jei_hide_fluids.js
@@ -0,0 +1,12 @@
+onEvent('jei.hide.fluids', (event) => {
+    event.hide('cofh_core:honey');
+    event.hide('create:honey');
+    event.hide('thermal:crude_oil');
+    event.hide('immersivepetroleum:oil');
+    event.hide('minecraft:water');
+    event.hide('minecraft:lava');
+    event.hide('minecraft:milk');
+    event.hide('biggerreactors:liquid_uranium');
+    event.hide('biggerreactors:liquid_obsidian');
+    event.hide('biggerreactors:steam');
+});
