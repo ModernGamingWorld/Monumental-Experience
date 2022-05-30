c41ccdebe9a0d49390f9ebbc16367108dc24ba27
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-05-30 00:05:32 -0500

    Update create.js

--- 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/create.js
+++ 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/create.js
@@ -109,6 +109,7 @@ onEvent('recipes', e => {
     'create:smelting/lead_ingot_compat_thermal',
     'create:smelting/lead_ingot_compat_eidolon',
     'create:smelting/ingot_lead_compat_mekanism',
-    'create_stuff_additions:brasss_pickaxe_recipe'
+    'create_stuff_additions:brasss_pickaxe_recipe',
+    'create_stuff_additions:zinc_pickaxe_recipe'
   ])
 })
