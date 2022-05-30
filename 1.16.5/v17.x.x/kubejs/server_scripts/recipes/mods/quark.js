f777675918d227792226d7884681d9e35b6bde36
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-05-30 18:50:12 -0500

    Update quark.js

--- 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/quark.js
+++ 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/quark.js
@@ -1,6 +1,7 @@
 onEvent('recipes', e => {
   removeRecipeByID(e, [
     'quark:automation/crafting/obsidian_pressure_plate',
-    'quark:building/crafting/chests/chest_revert'
+    'quark:building/crafting/chests/chest_revert',
+    'quark:tweaks/crafting/utility/tools/stone_axe'
   ])
 })
