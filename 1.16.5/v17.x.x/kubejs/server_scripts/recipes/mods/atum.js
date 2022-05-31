9398e9cdfddfbf36fa3a6c9cba6d96ecf41a580f
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-05-30 21:45:44 -0500

    Update atum.js

--- 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/atum.js
+++ 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/atum.js
@@ -3,10 +3,6 @@ onEvent('recipes', e => {
 
 	removeRecipeByID(e, [
 		'atum:diamond_from_blasting',
-		'atum:limestone_pickaxe',
-		'atum:limestone_axe',
-		'atum:limestone_hoe',
-		'atum:limestone_sword',
 		'atum:limestone_pressure_plate',
 		'atum:palm_pressure_plate',
 		'atum:deadwood_pressure_plate',
