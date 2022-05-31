73238040f1ab99d4b45bd189d57366dd53888d58
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-05-31 17:20:12 -0500

    Update ars_nouveau.js

--- 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/ars_nouveau.js
+++ 1.16.5/v17.x.x/kubejs/server_scripts/recipes/mods/ars_nouveau.js
@@ -1,6 +1,7 @@
 onEvent('recipes', e => {
   removeRecipeByID(e, [
     'ars_nouveau:mana_gem_block',
-    'ars_nouveau:mana_gem_2'
+    'ars_nouveau:mana_gem_2',
+    'ars_nouveau:archwood_pressure_plate'
   ])
 })
