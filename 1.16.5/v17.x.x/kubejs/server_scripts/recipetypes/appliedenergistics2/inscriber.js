a00ff9841a254faa31875063b44455bb24ff8021
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-06-10 15:51:33 -0500

    Update inscriber.js

--- 1.16.5/v17.x.x/kubejs/server_scripts/recipetypes/appliedenergistics2/inscriber.js
+++ 1.16.5/v17.x.x/kubejs/server_scripts/recipetypes/appliedenergistics2/inscriber.js
@@ -13,14 +13,21 @@ onEvent('recipes', e => {
 	}
 
 	// Applied Energistics 2
-	inscriberRecipe('appliedenergistics2:printed_engineering_processor', 'appliedenergistics2:engineering_processor_press', '#forge:plates/enderium', 'appliedenergistics2:printed_silicon')
-	inscriberRecipe('appliedenergistics2:printed_logic_processor', 'appliedenergistics2:logic_processor_press', '#forge:plates/electrum', 'appliedenergistics2:printed_silicon')
-	inscriberRecipe('appliedenergistics2:printed_calculation_processor', 'appliedenergistics2:calculation_processor_press', '#appliedenergistics2:crystals/certus', 'appliedenergistics2:printed_silicon')
+	inscriberRecipe('appliedenergistics2:logic_processor', 'appliedenergistics2:printed_logic_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
+	inscriberRecipe('appliedenergistics2:calculation_processor', 'appliedenergistics2:printed_calculation_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
+	inscriberRecipe('appliedenergistics2:engineering_processor', 'appliedenergistics2:printed_engineering_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
+	inscriberRecipe('appliedenergistics2:printed_engineering_processor', 'appliedenergistics2:engineering_processor_press', '#forge:plates/enderium', '#forge:gems/silicon')
+	inscriberRecipe('appliedenergistics2:printed_logic_processor', 'appliedenergistics2:logic_processor_press', '#forge:plates/electrum', '#forge:gems/silicon')
+	inscriberRecipe('appliedenergistics2:printed_calculation_processor', 'appliedenergistics2:calculation_processor_press', '#appliedenergistics2:crystals/certus', '#forge:gems/silicon')
 
 	// Extra Disks
-	inscriberRecipe('extradisks:withering_processor', 'extradisks:raw_withering_processor', '#forge:dusts/redstone', 'appliedenergistics2:printed_silicon')
+	inscriberRecipe('extradisks:withering_processor', 'extradisks:raw_withering_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
+
+	// Extra Storage
+	inscriberRecipe('extrastorage:neural_processor', 'extrastorage:raw_neural_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
 
 	// Refined Storage
-	inscriberRecipe('refinedstorage:advanced_processor', 'refinedstorage:raw_advanced_processor', '#forge:dusts/redstone', '#forge:gems/silicon')
-	inscriberRecipe('refinedstorage:basic_processor', 'refinedstorage:raw_basic_processor', '#forge:dusts/redstone', '#forge:gems/silicon')
+	inscriberRecipe('refinedstorage:advanced_processor', 'refinedstorage:raw_advanced_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
+	inscriberRecipe('refinedstorage:basic_processor', 'refinedstorage:raw_basic_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
+	inscriberRecipe('refinedstorage:improved_processor', 'refinedstorage:raw_improved_processor', 'extendedcrafting:redstone_component', '#forge:gems/silicon')
 })
