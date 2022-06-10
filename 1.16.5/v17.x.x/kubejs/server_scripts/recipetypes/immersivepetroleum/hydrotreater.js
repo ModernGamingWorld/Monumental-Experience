f273b93991d728c0ed78e6e39fe7b2074a98e1f6
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-06-09 21:48:39 -0500

    Update hydrotreater.js

--- 1.16.5/v17.x.x/kubejs/server_scripts/recipetypes/immersivepetroleum/hydrotreater.js
+++ 1.16.5/v17.x.x/kubejs/server_scripts/recipetypes/immersivepetroleum/hydrotreater.js
@@ -5,7 +5,7 @@ onEvent('recipes', e => {
 		energy: 512,
 		result: Fluid.of('immersivepetroleum:diesel', 7).toJson(),
 		input: Fluid.of('#forge:diesel_sulfur', 7).toJson(),
-		secondary_input: Fluid.of('#minecraft:water', 7).toJson(),
+		secondary_input: Fluid.of('minecraft:water', 7).toJson(),
 		secondary_result: Item.of('emendatusenigmatica:sulfur_dust').withChance(0.02).toJson()
 	}).id('immersivepetroleum:hydrotreater/sulfur_recovery')
 })
