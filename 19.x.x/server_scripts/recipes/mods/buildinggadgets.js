6ff837d608fae73365ddd070dff2a756fb0f6d20
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-11 20:13:42 -0500

    Create buildinggadgets.js

---
+++ 19.x.x/server_scripts/recipes/mods/buildinggadgets.js
@@ -0,0 +1,39 @@
+ServerEvents.recipes(e => {
+  e.shaped('buildinggadgets:gadget_building', ['AEA', 'DBD', 'ACA'], {
+    A: '#forge:plates/iron',
+    B: 'immersiveengineering:insulating_glass',
+    C: '#forge:gems/lapis',
+    D: '#forge:gears/diamond',
+    E: 'create:electron_tube'
+  })
+  e.shaped('buildinggadgets:gadget_copy_paste', ['AEA', 'DBD', 'ACA'], {
+    A: '#forge:plates/iron',
+    B: 'immersiveengineering:insulating_glass',
+    C: '#forge:gems/lapis',
+    D: '#forge:gems/emerald',
+    E: 'create:electron_tube'
+  })
+  e.shaped('buildinggadgets:gadget_destruction', ['AEA', 'DBD', 'ACA'], {
+    A: '#forge:plates/iron',
+    B: 'immersiveengineering:insulating_glass',
+    C: '#forge:plates/lapis',
+    D: '#forge:dusts/ender_pearl',
+    E: 'create:electron_tube'
+  })
+  e.shaped('buildinggadgets:gadget_exchanging', ['AEA', 'DBD', 'ACA'], {
+    A: '#forge:plates/iron',
+    B: 'immersiveengineering:insulating_glass',
+    C: '#forge:gems/arcane_crystal',
+    D: '#forge:gems/diamond',
+    E: 'create:electron_tube'
+  })
+
+  removeRecipeByID(e, [
+    'buildinggadgets:construction_block_powder',
+    'buildinggadgets:construction_paste_powder',
+    'buildinggadgets:gadget_building',
+    'buildinggadgets:gadget_copy_paste',
+    'buildinggadgets:gadget_destruction',
+    'buildinggadgets:gadget_exchanging'
+  ])
+})
