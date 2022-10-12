dbf7cdb17e8c2e50da6f3588e146109ac93b5473
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-11 21:27:58 -0500

    Create fluxnetworks.js

---
+++ 19.x.x/server_scripts/recipes/mods/fluxnetworks.js
@@ -0,0 +1,23 @@
+ServerEvents.recipes(e => {
+  e.shaped('fluxnetworks:flux_plug', ['xcx', 'cbc', 'xcx'], {
+    c: 'fluxnetworks:flux_core',
+    b: 'fluxnetworks:flux_block',
+    x: 'pneumaticcraft:plastic'
+  })
+  e.shaped('fluxnetworks:flux_configurator', [' fe', ' of', 'o  '], {
+    o: '#forge:ingots/obsidian',
+    f: 'fluxnetworks:flux_dust',
+    e: 'fluxnetworks:flux_core'
+  })
+
+  removeRecipeByID(e, [
+    'fluxnetworks:basicfluxstorage',
+    'fluxnetworks:gargantuanfluxstorage',
+    'fluxnetworks:herculeanfluxstorage',
+    'fluxnetworks:fluxconfigurator',
+    'fluxnetworks:fluxcontroller',
+    'fluxnetworks:fluxcore',
+    'fluxnetworks:fluxplug',
+    'fluxnetworks:fluxpoint'
+  ])
+})
