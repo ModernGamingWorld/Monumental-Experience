d72f6bf85273a745a56412178f1d020b657e363c
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-12 19:09:50 -0500

    Update pneumaticcraft.js

--- 1.16.5/v18.x.x/kubejs/server_scripts/recipes/mods/pneumaticcraft.js
+++ 1.16.5/v18.x.x/kubejs/server_scripts/recipes/mods/pneumaticcraft.js
@@ -53,15 +53,16 @@ onEvent('recipes', e => {
 
   removeRecipeByID(e, [
     'pneumaticcraft:reinforced_stone',
+    'pneumaticcraft:compressed_iron_helmet',
     'pneumaticcraft:compressed_iron_chestplate',
+    'pneumaticcraft:compressed_iron_leggings',
+    'pneumaticcraft:compressed_iron_boots',
     'pneumaticcraft:pressure_chamber/capacitor',
     'pneumaticcraft:pressure_chamber/empty_pcb',
     'pneumaticcraft:pressure_chamber/transistor',
     'pneumaticcraft:pressure_chamber/compressed_iron_ingot',
     'pneumaticcraft:explosion_crafting/compressed_iron_ingot',
     'pneumaticcraft:empty_pcb_from_failed_pcb',
-    'pneumaticcraft:compressed_iron_helmet',
-    'pneumaticcraft:compressed_iron_leggings',
     'pneumaticcraft:compressed_iron_gear',
     'pneumaticcraft:air_compressor',
     'pneumaticcraft:gps_tool',
