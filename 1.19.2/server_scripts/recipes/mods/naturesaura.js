29d4c3c6e0f3c452675962f5d8be6eedda7a343e
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-12 19:23:23 -0500

    Create naturesaura.js

---
+++ 1.19.2/server_scripts/recipes/mods/naturesaura.js
@@ -0,0 +1,28 @@
+ServerEvents.recipes(e => {
+
+  removeRecipeByID(e, [
+    'naturesaura:offering_table',
+    'naturesaura:altar/tainted_gold_block',
+    'naturesaura:infused_iron_block',
+    'naturesaura:altar/tainted_gold',
+    'naturesaura:altar/infused_iron',
+    'naturesaura:sky_helmet',
+    'naturesaura:sky_chest',
+    'naturesaura:sky_pants',
+    'naturesaura:sky_shoes',
+    'naturesaura:sky_pickaxe',
+    'naturesaura:sky_axe',
+    'naturesaura:sky_shovel',
+    'naturesaura:sky_hoe',
+    'naturesaura:sky_sword',
+    'naturesaura:infused_pickaxe',
+    'naturesaura:infused_axe',
+    'naturesaura:infused_shovel',
+    'naturesaura:infused_hoe',
+    'naturesaura:infused_iron_chest',
+    'naturesaura:infused_chest',
+    'naturesaura:infused_pants',
+    'naturesaura:infused_sword',
+    'naturesaura:altar/infused_stone'
+  ])
+})
