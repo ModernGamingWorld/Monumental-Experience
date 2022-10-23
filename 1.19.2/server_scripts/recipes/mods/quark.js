4473d7767965c4e9701b6da06518e2e5de1648ae
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-23 17:59:45 -0500

    Update quark.js

--- 1.19.2/server_scripts/recipes/mods/quark.js
+++ 1.19.2/server_scripts/recipes/mods/quark.js
@@ -1,8 +1,8 @@
 ServerEvents.recipes(e => {
   removeRecipeByID(e, [
     'quark:automation/crafting/obsidian_pressure_plate',
-    'quark:automation/crafting/woodsets/blossom/pressure_plate',
-    'quark:automation/crafting/woodsets/azalea/pressure_plate',
+    'quark:world/crafting/woodsets/blossom/pressure_plate',
+    'quark:world/crafting/woodsets/azalea/pressure_plate',
     'quark:building/crafting/chests/chest_revert',
     'quark:tweaks/crafting/utility/tools/stone_axe',
     'quark:tweaks/crafting/utility/tools/stone_sword',
