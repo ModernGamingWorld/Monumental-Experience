74b572df14dff1595ac1c50059b57ff0e5f683a2
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-23 17:32:55 -0500

    Create industrialforegoing.js

---
+++ 1.19.2/server_scripts/recipes/mods/industrialforegoing.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes(e => {
+  removeRecipeByID(e, [
+    'industrialforegoing:machine_frame_pity',
+    'industrialforegoing:dissolution_chamber/infinity_nuke',
+    'industrialforegoing:fluid_extractor',
+    'industrialforegoing:latex_processing_unit',
+    'industrialforegoing:iron_gear',
+    'industrialforegoing:gold_gear',
+    'industrialforegoing:diamond_gear'
+  ])
+})
