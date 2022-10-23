ebcaadd452d34b211977ba6c86b21c8f4f404fbb
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-23 17:32:51 -0500

    Create hexerei.js

---
+++ 1.19.2/server_scripts/recipes/mods/hexerei.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(e => {
+  removeRecipeByID(e, [
+    'hexerei:polished_mahogany_pressure_plate',
+    'hexerei:mahogany_pressure_plate',
+    'hexerei:polished_willow_pressure_plate',
+    'hexerei:willow_pressure_plate'
+  ])
+})
