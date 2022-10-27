381d34c4c5cea3393f704ce525748b4023d11847
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-27 00:18:13 -0500

    Update mininggadgets.js

--- 1.19.2/server_scripts/recipes/mods/mininggadgets.js
+++ 1.19.2/server_scripts/recipes/mods/mininggadgets.js
@@ -1,5 +1,7 @@
 ServerEvents.recipes(e => {
   removeRecipeByID(e, [
+    'mininggadgets:mininggadget_simple',
+    'mininggadgets:mininggadget_fancy',
     'mininggadgets:mininggadget',
     'mininggadgets:upgrade_empty',
     'mininggadgets:modificationtable'
