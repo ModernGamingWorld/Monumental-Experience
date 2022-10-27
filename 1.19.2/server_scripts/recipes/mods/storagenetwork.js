47c48324cde48fc044af469ae67af4b677906faf
ModernGamingWorld <75923060+ModernGamingWorld@users.noreply.github.com>
2022-10-27 00:18:17 -0500

    Update storagenetwork.js

--- 1.19.2/server_scripts/recipes/mods/storagenetwork.js
+++ 1.19.2/server_scripts/recipes/mods/storagenetwork.js
@@ -5,10 +5,23 @@ ServerEvents.recipes(e => {
     'storagenetwork:exchange',
     'storagenetwork:inventory',
     'storagenetwork:inventory_remote',
+    'storagenetwork:picker_remote',
+    'storagenetwork:collector_remote',
+    'storagenetwork:builder_remote',
+    'storagenetwork:export_kabel',
+    'storagenetwork:filter_kabel',
+    'storagenetwork:import_filter_kabel',
+    'storagenetwork:import_kabel',
+    'storagenetwork:storage_kabel',
     'storagenetwork:request',
     'storagenetwork:speed_upgrade',
     'storagenetwork:kabel',
     'storagenetwork:stack_upgrade',
+    'storagenetwork:slow_upgrade',
+    'storagenetwork:stock_upgrade',
+    'storagenetwork:operation_upgrade',
+    'storagenetwork:single_upgrade',
+    'storagenetwork:stock_upgrade',
     'storagenetwork:master'
   ])
 })
